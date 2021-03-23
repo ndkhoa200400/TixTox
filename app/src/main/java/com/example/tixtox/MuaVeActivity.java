76d2a6e6a11e4ea3f618a56fe883c98e850ccb3e
Ly Dang Huy <lydanghuy772000@gmail.com>
2021-03-23 15:05:59 +0700

    Layout Ban Ve

---
+++ app/src/main/java/com/example/tixtox/MuaVeActivity.java
@@ -0,0 +1,124 @@
+package com.example.tixtox;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.media.Image;
+import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.BaseAdapter;
+import android.widget.GridLayout;
+import android.widget.GridView;
+import android.widget.ImageView;
+import android.widget.ListAdapter;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class MuaVeActivity extends AppCompatActivity {
+    GridView gridview;
+    private ArrayList<ImageModel> arrayList;
+
+    Integer[] image = {R.drawable.icon_ghe,R.drawable.icon_ghe,R.drawable.icon_ghe,
+            R.drawable.icon_ghe,R.drawable.icon_ghe,
+            R.drawable.icon_ghe,R.drawable.icon_ghe,
+            R.drawable.icon_ghe,R.drawable.icon_ghe,
+            R.drawable.icon_ghe,R.drawable.icon_ghe,
+            R.drawable.icon_ghe,R.drawable.icon_ghe,
+            R.drawable.icon_ghe,R.drawable.icon_ghe,
+            R.drawable.icon_ghe,R.drawable.icon_ghe,
+            R.drawable.icon_ghe,R.drawable.icon_ghe,
+            R.drawable.icon_ghe,R.drawable.icon_ghe,
+            R.drawable.icon_ghe,R.drawable.icon_ghe,
+            R.drawable.icon_ghe,R.drawable.icon_ghe,
+            R.drawable.icon_ghe,R.drawable.icon_ghe,
+            R.drawable.icon_ghe,R.drawable.icon_ghe,
+            R.drawable.icon_ghe,R.drawable.icon_ghe,
+            R.drawable.icon_ghe,R.drawable.icon_ghe,
+            R.drawable.icon_ghe,R.drawable.icon_ghe,
+            R.drawable.icon_ghe,R.drawable.icon_ghe,
+            R.drawable.icon_ghe,R.drawable.icon_ghe,
+            R.drawable.icon_ghe,R.drawable.icon_ghe,
+            R.drawable.icon_ghe,R.drawable.icon_ghe,
+            R.drawable.icon_ghe,R.drawable.icon_ghe,
+            R.drawable.icon_ghe,R.drawable.icon_ghe,
+            R.drawable.icon_ghe,R.drawable.icon_ghe,
+            R.drawable.icon_ghe,R.drawable.icon_ghe,
+            R.drawable.icon_ghe,R.drawable.icon_ghe,
+            R.drawable.icon_ghe,R.drawable.icon_ghe,
+            R.drawable.icon_ghe,R.drawable.icon_ghe,
+            R.drawable.icon_ghe,R.drawable.icon_ghe,
+            R.drawable.icon_ghe,R.drawable.icon_ghe,
+            R.drawable.icon_ghe,R.drawable.icon_ghe,
+            R.drawable.icon_ghe,R.drawable.icon_ghe,
+            R.drawable.icon_ghe,R.drawable.icon_ghe,
+            R.drawable.icon_ghe,R.drawable.icon_ghe,
+            R.drawable.icon_ghe,R.drawable.icon_ghe,
+            R.drawable.icon_ghe,R.drawable.icon_ghe,
+            R.drawable.icon_ghe,R.drawable.icon_ghe,
+            R.drawable.icon_ghe,R.drawable.icon_ghe,
+            R.drawable.icon_ghe,R.drawable.icon_ghe,
+            R.drawable.icon_ghe,R.drawable.icon_ghe,
+            R.drawable.icon_ghe,R.drawable.icon_ghe,
+            R.drawable.icon_ghe,R.drawable.icon_ghe,
+            R.drawable.icon_ghe,R.drawable.icon_ghe,
+            R.drawable.icon_ghe
+    };
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        gridview = (GridView) findViewById(R.id.gridviewGhe);
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_muave);
+        gridview = (GridView) findViewById(R.id.gridviewGhe);
+
+
+        gridview.setAdapter(new ImageAdapterGridView(this));
+    }
+    private  class ImageAdapterGridView extends BaseAdapter
+    {
+        private Context mcontext;
+        public ImageAdapterGridView(Context context)
+        {
+            mcontext = context;
+        }
+
+        @Override
+        public int getCount() {
+            return image.length;
+        }
+
+        @Override
+        public Object getItem(int position) {
+            return null;
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return 0;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            ImageView imageView;
+            if(convertView == null) {
+                imageView = new ImageView(mcontext);
+                imageView.setLayoutParams(new GridView.LayoutParams(80,100));
+                imageView.setScaleType(ImageView.ScaleType.CENTER_CROP);
+                imageView.setPadding(16,10,16,10);
+
+            }else
+            {
+                imageView = (ImageView) convertView;
+            }
+            imageView.setImageResource(image[position]);
+            return imageView;
+        }
+
+    }
+
+
+
+}
